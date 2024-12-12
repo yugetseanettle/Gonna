0a4cc9b69ec6d399bf5cf4d4fdf5ef093e7ca9d9
yugetseanettle <yuget.seanettle@gmail.com>
2024-12-12 14:44:05 +0900

    nextconfig add env variables

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+  },
 };
 
 export default nextConfig;
