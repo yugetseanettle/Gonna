1a403bb4ec36979ac88387861a6d7ca94c2cbbc6
yugetseanettle <yuget.seanettle@gmail.com>
2024-12-12 14:51:11 +0900

    chage way of getting firebase config

--- src/lib/firebase/Firebase.ts
+++ src/lib/firebase/Firebase.ts
@@ -9,14 +9,23 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBD2YBdjN3PSzIabLaaSIyO8saBX1kVIH4",
+  authDomain: "gonna-calendar.firebaseapp.com",
+  projectId: "gonna-calendar",
+  storageBucket: "gonna-calendar.firebasestorage.app",
+  messagingSenderId: "694583284436",
+  appId: "1:694583284436:web:9cbc80f86485b815e2e141",
+  measurementId: "G-B7X3EG7Q52",
 };
 
 // Initialize Firebase
