d9cf0528979db6a238fb0bc64e5a1a1029fff672
yugetseanettle <yuget.seanettle@gmail.com>
2024-12-12 21:16:09 +0900

    delete apikey for firebaseconfig

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
-  },
 };
 
 export default nextConfig;
